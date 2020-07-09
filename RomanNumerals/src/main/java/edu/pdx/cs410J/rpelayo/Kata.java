8fdce73a2b5a8b6d39839052ea2c5eb58c194e88
Xingjian <xingjian@pdx.edu>
2020-07-08 20:14:10 -0700

    Add Roma num form 1 to 5

--- RomanNumerals/src/main/java/edu/pdx/cs410J/rpelayo/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/rpelayo/Kata.java
@@ -15,6 +15,31 @@ public class Kata {
   }
 
   public static String romanNumeralFor(int number){
-    return "I";
+    /*
+    I(1)，V(5)，X(10)，L(50)，C(100)，D(500)，M(1000)
+     */
+    public String intToRoman(int num) {
+      int list[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+      String chars[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+      int i = 0;
+      String out="";
+      while(num > 0)
+      {
+        for(;i < list.length;i++)
+          if(num >= list[i])
+            break;
+        out+=chars[i];
+        num -= list[i];
+      }
+      return out;
+    }
+
+    String[] Roma={"I","II","III","IV","V"};
+    for(int i=0;i<10;i++){
+      if(i+1==number){
+        return Roma[i];
+      }
+    }
+    return null;
   }
 }
--- RomanNumerals/src/test/java/edu/pdx/cs410J/rpelayo/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/rpelayo/KataTest.java
@@ -11,8 +11,25 @@ public class KataTest
 
   @Test
   public void romanNumeralFor1IsI() {
+
     assertThat(Kata.romanNumeralFor(1), equalTo("I"));
   }
+  @Test
+  public void romanNumeralFor1IsII() {
+
+    assertThat(Kata.romanNumeralFor(2), equalTo("II"));
+  }
+  @Test
+  public void romanNumeralFor1IsIV() {
+
+    assertThat(Kata.romanNumeralFor(4), equalTo("IV"));
+  }
+  @Test
+  public void romanNumeralFor1IsV() {
+
+    assertThat(Kata.romanNumeralFor(5), equalTo("V"));
+  }
+
 
 
 }
